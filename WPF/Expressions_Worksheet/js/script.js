42816c5b03cd33b5c8121317b5e8564eea02556d
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-15 15:49:22 -0400

    Second Problem and solution added

--- WPF/Expressions_Worksheet/js/script.js
+++ WPF/Expressions_Worksheet/js/script.js
@@ -3,3 +3,14 @@
 var DAPY = 32;   //Given: Dogs age in people age
 var DADY = DAPY*7;   //Calculate the Variable "DAPY" by 7 to define dogs age in "Dog Years"
 console.log("Sparky is "+ DAPY +" human years old which is "+DADY +" in dog years."); // Print a report of both ages with explination
+
+//Slice of Pie Part 1
+
+//Given Items
+
+var pizzas = 20 //Represents total number of pizzas ordered
+var inclSlice = (pizzas*8)  //Calculate total number of slices in the order
+var screamingBaby = 3 // Number of guests at the party
+
+var result = (inclSlice/screamingBaby) //devide the number of slices evenly among the number of guests
+console.log(result)   //Display the number of slices each guest may eat
