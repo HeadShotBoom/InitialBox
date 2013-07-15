056a901414ae1b4d4dde6c030fe297855065fd0f
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-15 16:17:18 -0400

    Solved Grocery Problem

--- WPF/Expressions_Worksheet/js/script.js
+++ WPF/Expressions_Worksheet/js/script.js
@@ -8,20 +8,21 @@ console.log("Sparky is "+ DAPY +" human years old which is "+DADY +" in dog year
 
 //Given Items
 
-var pizzas = 20 //Represents total number of pizzas ordered
-var inclSlice = (pizzas*8)  //Calculate total number of slices in the order
-var screamingBaby = 3 // Number of guests at the party
+var pizzas = 20; //Represents total number of pizzas ordered
+var inclSlice = (pizzas*8);  //Calculate total number of slices in the order
+var screamingBaby = 3; // Number of guests at the party
 
-var result = (inclSlice/screamingBaby) //devide the number of slices evenly among the number of guests
-console.log("Each person ate "+result+" slices of pizza at the party.")   //Display the number of slices each guest may eat
+var result = (inclSlice/screamingBaby); //devide the number of slices evenly among the number of guests
+console.log("Each person ate "+result+" slices of pizza at the party.");   //Display the number of slices each guest may eat
 
 //Slice of Pie Part 2
 
-var sparky =(inclSlice%screamingBaby) //Calculates the remainder left after dividing the 1st variable by the second variable
-console.log ("Sparky got "+sparky+" slices of pizza.")   //Prints the value to the console log
+var sparky =(inclSlice%screamingBaby); //Calculates the remainder left after dividing the 1st variable by the second variable
+console.log ("Sparky got "+sparky+" slices of pizza.");   //Prints the value to the console log
 
 //Average shopping bill
 
-var weeksBills [140, 220, 1254, 23, 130] //List of grocery bills month at a time for the previous 5 months
-var total = weeksBills //Add all bills to single total
-var average = (weeksBills/5) //Average bill of 5 months
+var weeksBills = [140, 220, 1254, 23, 130]; //List of grocery bills month at a time for the previous 5 months
+var total = weeksBills[0]+weeksBills[1]+weeksBills[2]+weeksBills[3]+weeksBills[4]; //Add all bills to single total
+var average = (total/5); //Average bill of 5 months
+console.log("You have spent a total of $"+ total+" on groceries over 5 weeks. That is an average of $"+average+" per week.");//Print Result
