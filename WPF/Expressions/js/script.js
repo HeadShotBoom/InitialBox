7561e280a5d5f409ee7bc4d8bcf639323c8eccb9
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-15 19:20:12 -0400

    Concatenation!!!

--- WPF/Expressions/js/script.js
+++ WPF/Expressions/js/script.js
@@ -1,7 +1,4 @@
 // Basic Expressions
 
-//Using Arrays as expressions
+//Concatenating, Combining Strings
 
-var orangeBins = [234, 324, 123];
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2]
-console.log(total)
