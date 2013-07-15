0df3d6521ff1fb81f226ccc73906f908c329f079
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-15 18:03:23 -0400

    Moved on to next lesson

--- WPF/Expressions/js/script.js
+++ WPF/Expressions/js/script.js
@@ -1,5 +1,3 @@
 // Basic Expressions
 
-var a = 2               //sets up variable and defines it
-a = a+3               //Adds 3 to the value of a then updates a with new total
-console.log(a)       //  Prints result
+//How Old Are You
