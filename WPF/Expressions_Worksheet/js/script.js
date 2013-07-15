439c6c91aa058e2947de21e34ba8c3de7f872c09
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-15 15:20:09 -0400

    Comment added

--- WPF/Expressions_Worksheet/js/script.js
+++ WPF/Expressions_Worksheet/js/script.js
@@ -1,4 +1,4 @@
 //Calculte Dog Age
 
 var DAPY = 32   //Given: Dogs age in people age
-var DADY = DAPY*7   //
+var DADY = DAPY*7   //Calculate the Variable "DAPY" by 7 to define dogs age in "Dog Years"
