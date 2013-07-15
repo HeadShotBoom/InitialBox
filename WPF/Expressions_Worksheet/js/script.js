cf945330b6f0a97e0f6b3abf03162fd7f54312f3
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-15 16:02:52 -0400

    Next problem added

--- WPF/Expressions_Worksheet/js/script.js
+++ WPF/Expressions_Worksheet/js/script.js
@@ -13,9 +13,13 @@ var inclSlice = (pizzas*8)  //Calculate total number of slices in the order
 var screamingBaby = 3 // Number of guests at the party
 
 var result = (inclSlice/screamingBaby) //devide the number of slices evenly among the number of guests
-console.log(result)   //Display the number of slices each guest may eat
+console.log("Each person ate "+result+" slices of pizza at the party.")   //Display the number of slices each guest may eat
 
 //Slice of Pie Part 2
 
 var sparky =(inclSlice%screamingBaby) //Calculates the remainder left after dividing the 1st variable by the second variable
-console.log (sparky)   //Prints the value to the console log
+console.log ("Sparky got "+sparky+" slices of pizza.")   //Prints the value to the console log
+
+//Average shopping bill
+
+var weeksBills [140, 220, 1254, 23, 130] //List of grocery bills month at a time for the previous 5 months
