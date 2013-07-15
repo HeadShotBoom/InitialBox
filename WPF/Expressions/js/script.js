510acc2bee07b7d57091e187094651f8e9bb12ef
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-15 19:14:06 -0400

    New Task

--- WPF/Expressions/js/script.js
+++ WPF/Expressions/js/script.js
@@ -1,19 +1,3 @@
 // Basic Expressions
 
-//Order of operations
-
-var quiz1 = 80;
-var quiz2 = 100;
-var quiz3 = 97;
-var quiz4 = 2;
-var quiz5 = 99;
-
-//the average is the sum of the items divide by their total number
-
-var average = (quiz1 + quiz2 + quiz3 + quiz4 + quiz5)/5;
-//console.log(average)
-
-var length = 7;
-var width = 6;
-var peremiter = length * 2 + width * 2
-console.log(peremiter)
+//Using Arrays as expressions
