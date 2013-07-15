baf1bcf772a940fb6466d4cabd84210b1cfec7fc
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-15 19:08:58 -0400

    adding it up

--- WPF/Expressions/js/script.js
+++ WPF/Expressions/js/script.js
@@ -1,3 +1,14 @@
 // Basic Expressions
 
 //Order of operations
+
+var quiz1 = 80;
+var quiz2 = 100;
+var quiz3 = 97;
+var quiz4 = 2;
+var quiz5 = 99;
+
+//the average is the sum of the items divide by their total number
+
+var average = quiz1 + quiz2 + quiz3 + quiz4 + quiz5;
+console.log(average)
