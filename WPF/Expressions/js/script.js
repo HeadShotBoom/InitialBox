a4183358e345f7f9120bc8a154bb1407bade4067
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-15 17:54:03 -0400

    Update a variable

--- WPF/Expressions/js/script.js
+++ WPF/Expressions/js/script.js
@@ -1,4 +1,5 @@
 // Basic Expressions
 
 var a = 2               //sets up variable and defines it
+a = a+3               //Adds 3 to the value of a then updates a with new total
 console.log(a)       //  Prints result
