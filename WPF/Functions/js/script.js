50a6d05dbd44944ae8e4095c698bc74602cc300d
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-17 10:36:21 -0400

    Working on Classwork

--- WPF/Functions/js/script.js
+++ WPF/Functions/js/script.js
@@ -2,31 +2,44 @@
 
 console.log("---------Function---------");
 
-function helloMsg(){
-      console.log("Hello World!");
-}
-helloMsg();
+//function helloMsg(){
+//      console.log("Hello World!");
+//}
+//helloMsg();
 
-function shoutOut(){
-    console.log("AAAHHHHHHHH");
-}
+//function shoutOut(){
+//    console.log("AAAHHHHHHHH");
+//}
 
-function twoMessage(){
-    console.log("Message 1");
-    console.log("Message 2");
-}
-twoMessage()
+//function twoMessage(){
+//    console.log("Message 1");
+//    console.log("Message 2");
+//}
+//twoMessage()
 
-function combineItAll(){
-helloMsg()
-twoMessage()
-}
+//function combineItAll(){
+//helloMsg()
+//twoMessage()
+//}
 
-combineItAll()
+//combineItAll()
 
 console.log("---------Scope-------------");
+//function calArea(){
+//    var width = 10;
+//    var height = 15;
+//    var area = width * height;
+//    console.log(area);
+//}
+//calArea();
 
 console.log("---Arguements and paremeters---");
+
+function calculateArea(W, H){
+    var area = W * H;
+    console.log(area);
+}
+calculateArea(20,30)
 
 console.log("----Return Function------");
 
@@ -35,3 +48,5 @@ console.log("------Annonymous Function------");
 function helloMsg (){
     console.log("Hello World");
 }
+
+helloMsg();
