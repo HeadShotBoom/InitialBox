39d32f1804fa94fabb81684e38d05396541931c4
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-19 10:00:24 -0400

    Progress

--- WPF/Loops N Such/js/script.js
+++ WPF/Loops N Such/js/script.js
@@ -1,2 +1,21 @@
 // Script
 
+
+//var charAt = ['Test', 'test2', 'test3'];
+//console.log(charAt);
+//
+//if(charAt[0]==''){
+//    console.log('True');
+//}else{
+//    console.log('False')
+//}
+//
+//
+//for(i=0;i<10;i++){
+//    console.log(charAt[i]);
+//}
+
+var kids = [12, 14, 8, 14, 9, 23, 21, 3, 17, 18];
+
+for(i=0;i<10;i++)
+console.log(kids[i])
