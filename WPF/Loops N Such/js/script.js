0d0faf50154d0846f18e14258bc37464fd9b1bb7
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-19 10:18:25 -0400

    I understand!

--- WPF/Loops N Such/js/script.js
+++ WPF/Loops N Such/js/script.js
@@ -15,7 +15,10 @@
 //    console.log(charAt[i]);
 //}
 
-var kids = [12, 14, 8, 14, 9, 23, 21, 3, 17, 18];
-
-for(i=0;i<10;i++)
-console.log(kids[i])
+var kids = [12, 14, 8, 14, 9, 23, 21, 3, 17, 18, 100, 100, 100, 100, 100, 100, 100, 100, 100];
+var total =0;
+for(i=0;i<kids.length;i++){
+//    console.log(kids[i])
+    total +=kids[i]
+}
+console.log(total)
