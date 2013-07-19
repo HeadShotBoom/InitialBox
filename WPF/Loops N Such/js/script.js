f1b2d59493a0052ee6572960788e37a03f652fdd
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-19 10:29:32 -0400

    Adding with function

--- WPF/Loops N Such/js/script.js
+++ WPF/Loops N Such/js/script.js
@@ -15,10 +15,19 @@
 //    console.log(charAt[i]);
 //}
 
-var kids = [12, 14, 8, 14, 9, 23, 21, 3, 17, 18, 100, 100, 100, 100, 100, 100, 100, 100, 100];
-var total =0;
-for(i=0;i<kids.length;i++){
-//    console.log(kids[i])
-    total +=kids[i]
+//var kids = [12, 14, 8, 14, 9, 23, 21, 3, 17, 18, 100, 100, 100, 100, 100, 100, 100, 100, 100];
+//var total =0;
+//for(i=0;i<kids.length;i++){
+////    console.log(kids[i])
+//    total +=kids[i]
+//}
+//console.log(total)
+
+function junction(){
+    var array = [12,14,8,18];
+    var total =0;
+    for(i=0;i<array.length;i++)
+    total +=array[i]
+    console.log(total)
 }
-console.log(total)
+junction()
