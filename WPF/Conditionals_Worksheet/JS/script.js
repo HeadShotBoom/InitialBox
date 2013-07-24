196f7d94e4957baeefa2b2ecdd0859ba813656cd
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-24 17:16:04 -0400

    Incrimental Save

---
+++ WPF/Conditionals_Worksheet/JS/script.js
@@ -0,0 +1,28 @@
+//Celsius to Fahrenheit converter
+/*
+Convert a temperature to either degrees Celsius or degrees Fahrenheit depending on what the user has entered. If the user puts a “C” for the unit, the calculator should convert to Celsius. If the user puts “F” for the unit, the calculator should convert to Fahrenheit.
+
+
+    Given:
+Degrees (in F or C)
+Unit (a string holding an “F” or a “C”)
+Result:
+    “The temperature is X degrees Celsius.” Or “The temperature is X degrees Fahrenheit.”
+Data Sets to Test: (Note that data sets are not the only numbers that should work with your code.)
+32F is 0C
+100C is 212F
+90F is 32.22C  */
+
+// I will build a system of conditions and functions that is prompt driven to convert temperature.
+function cTOf(C,F){
+    var currentTemp =prompt("What is the temperature in degrees Celcius? (Please type only numbers.)");
+}
+
+function fTOc
+
+
+var convertType =prompt("What temp type are you converting TO? F or C (Please press F or C)");
+
+if (convertType=="C"){
+    fTOc
+}
