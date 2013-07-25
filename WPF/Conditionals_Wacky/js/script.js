c4bdcf44a9c60b7a3f5e0f6517f8466cfa25b019
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-25 18:48:13 -0400

    Added code for wacky and made comments

--- WPF/Conditionals_Wacky/js/script.js
+++ WPF/Conditionals_Wacky/js/script.js
@@ -1,2 +1,12 @@
 // Daniel Carroll, 25 July, Conditionals Assignment
 
+// I will make a set of conditionals that hopefully will pull off something wacky.
+//Informing the participant of what this site will do.
+alert("You will be asked to enter 3 values over the next 3 boxes. I will then do a secret math problem on those numbers. If the answer is positive we will make a line. If it is negative we will make a box.");
+//making a variable that will become loathed when it is used
+var statement = "Its time to waste some time!";
+//keeping with the wacky concept I made this loop.
+for(var i=0;i<3;i++){
+
+    alert(statement);
+}
