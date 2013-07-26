bded55f3b84224605414c2fe2993401610589929
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-26 14:54:55 -0400

    Update
    Adjusted function, added more animal types, adjusted all base
    characters values in anticipation of combat mechanics and initial
    expected balance issues.

--- WPF/GAME/js/creatures.js
+++ WPF/GAME/js/creatures.js
@@ -1,7 +1,14 @@
 // This will generate creatures to populate our world.
 
-/*
-
+//This function allows me to adjust the starting value of any stat dynamically
+function varyValue(N,M){
+    var rangeOfvariance = (Math.floor(Math.random()*100)+1)/100;
+    var posOrNeg = Math.floor((Math.random()*10)+1);
+    if (posOrNeg%2==0){
+        var variance = (N*(M/100)*rangeOfvariance)+N;
+    }else {var variance = ((N*(M/100)*rangeOfvariance)*-1)+N;}
+    return Math.floor(variance);
+}
 //**************************Remove this when testing is complete
 var arrayWithStatsPass=[];
 arrayWithStatsPass[0]='Monster';
@@ -9,61 +16,82 @@ arrayWithStatsPass[1]='Plains';
 //**************************Remove this when testing is complete
 function makeCreature()
 {
+
+
+
 var playerOrMonster=arrayWithStatsPass[0];
 if (playerOrMonster=='Player'){
     var stats=[];
-    //armor=0, health=1, damage=2, %toHit=3, %toDodge=4, exp=5
-    //starting armor value =0
-    stats[0]=0;
-    //starting health TBD
-    stats[1]=20;
-    //starting damage =1 (Punching)
-    stats[2]=1;
-    //starting %hit = 50%
-    stats[3]=0.5;
-    //starting %dodge = 1%
-    stats[4]=0.01;
-    ////starting experience =0
-    stats[5]=0;
-    // character name
-    stats[6]='PlayerCharacterName';
+    stats[0]=0;//starting armor value =0
+    stats[1]=20;//starting health TBD
+    stats[2]=1; //starting damage =1 (Punching)
+    stats[3]=0.5;//starting %hit = 50%
+    stats[4]=0.01;//starting %dodge = 1%
+    stats[5]=1;////starting experience =0
+    stats[6]='PlayerCharacterName'; // character name
 
 }
 if (playerOrMonster=='Monster'){
     var stats=[];
     switch(arrayWithStatsPass[1]){
         case "Swamp":
+            var animalTypeSwamp =Math.random();
+            if (animalTypeSwamp<.1){
             //Slimy sneaky slippery snake
-            stats[0]=0;
-            stats[1]=5;
-            stats[2]=2;
-            stats[3]=.2;
-            stats[4]=.2;
-            stats[5]=10;
-            stats[6]='Slimy sneaky slippery snake';
+            stats[0]=0;//starting armor value =0
+            stats[1]=5;//starting health
+            stats[2]=2;//starting damage
+            stats[3]=.2;//starting %hit
+            stats[4]=.2;//starting %dodge
+            stats[5]=1;//starting experience
+            stats[6]='Slimy sneaky slippery snake';// character name
             break;
+            }
+            else if (animalTypeSwamp>=.1 && animalTypeSwamp<=.9){
+            //Frog
+            stats[0]=0;//starting armor value =0
+            stats[1]=2;//starting health
+            stats[2]=2; //starting damage
+            stats[3]=.2;//starting %hit
+            stats[4]=.05;//starting %dodge
+            stats[5]=1;//starting experience
+            stats[6]='Sexy Toad';// character name
+            break;
+            }
+            else {
+            //Death Leach
+            stats[0]=0;//starting armor value =0
+            stats[1]=5;//starting health
+            stats[2]=.1;//starting damage
+            stats[3]=.9;//starting %hit
+            stats[4]=.991;//starting %dodge
+            stats[5]=1;//starting experience
+            stats[6]='Death Leach'; // character name
+            break;
+            }
+
         case "Plains":
             var dragonOrHorse =Math.random();
-            if (dragonOrHorse<.02){
+            if (dragonOrHorse<.01){
             //This case will be dragon
-                stats[0]=50;
-                stats[1]=105;
-                stats[2]=10;
-                stats[3]=.5;
-                stats[4]=0;
-                stats[5]=150;
-                stats[6]='Fat Sad Dragon';
+                stats[0]=50;//starting armor value =0
+                stats[1]=1000;//starting health
+                stats[2]=10; //starting damage
+                stats[3]=.5; //starting %hit
+                stats[4]=0;//starting %dodge
+                stats[5]=1;//starting experience
+                stats[6]='Fat Sad Dragon';// character name
                 break;
             }
             else {
             //this case makes a horse
-                stats[0]=0;
-                stats[1]=20;
-                stats[2]=4;
-                stats[3]=.1;
-                stats[4]=.1;
-                stats[5]=25;
-                stats[6]='A left wing horse of a different color';
+                stats[0]=0;//starting armor value =0
+                stats[1]=20;//starting health
+                stats[2]=4;  //starting damage
+                stats[3]=.1;//starting %hit
+                stats[4]=.1;//starting %dodge
+                stats[5]=1; //starting experience
+                stats[6]='A left wing horse of a different color';// character name
                 break;
             }
     }
@@ -79,15 +107,6 @@ console.log(playerCharacter[2]);
 console.log(playerCharacter[3]);
 console.log(playerCharacter[4]);
 console.log(playerCharacter[5]);
-             */
 
-function varyValue(N,M){
-    var points = N;
-    var randomize = Math.floor((Math.random()*100)+1);
-    var posOrNeg = Math.floor((Math.random()*10)+1);
-    if (posOrNeg%2==0){
-        var variance = points*(randomize/100)
-    }else {var variance =points*(randomize/100)*-1}
-    console.log(variance)
-}
-  varyValue();
+
+
