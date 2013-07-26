9f029f58380fa664e753e65da233b94d0909f7ab
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-25 20:16:20 -0400

    Entered some code and made a prompt

--- WPF/Conditionals_Industry/js/script.js
+++ WPF/Conditionals_Industry/js/script.js
@@ -1,2 +1,20 @@
 // Daniel Carroll, 25 July, Conditionals Assignment
 
+//I will build a set of conditionals that will determine what kind of team you will need to build based on the needs of various projects
+
+var perOne = "Graphic Artist";
+var perTwo = "Web Programmer";
+var perThree = "Photographer";
+var perFour = "Videographer";
+var perFive = "Project Manager";
+var perSix = "Models/Actors";
+
+
+alert("Please select a type of job from the ones described in console log.");
+    console.log("1) A client wants a website with videos of his product and a Bio page");
+    console.log("2) A client wants a website built so that he can operate an online car dealership.");
+    console.log("   It will need intro videos, photo tours of the cars, advertisements for the dealership and a secure commerce section.");
+    console.log("3) A client wants a website where he can host various graphics he can link to social media.");
+    console.log("   He also needs these graphics made professionally");
+
+var jobType =prompt("Please enter the number associated with your job. (Only enter 1, 2, or 3)");
