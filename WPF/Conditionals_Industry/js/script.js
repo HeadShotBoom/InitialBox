ca1ec713f5e9157c3f337e6b29eaf6aa2f789b8c
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-25 22:28:14 -0400

    Coding and testing complete

--- WPF/Conditionals_Industry/js/script.js
+++ WPF/Conditionals_Industry/js/script.js
@@ -20,32 +20,48 @@ alert("Please select a type of job from the ones described in console log.");
 var jobType =prompt("Please enter the number associated with your job. (Only enter 1, 2, or 3)");
 
 if (parseInt(jobType)==1){
-    var pics =prompt("Does the client need you to produce the pictures of his products? (Enter Y or N)");
+    var pics =prompt("Does the client need you to produce the pictures for his Bio page? (Enter Y or N)");
     var vid =prompt("Does the client need you to shoot the informational video?");
     if (vid.toUpperCase()=="Y"||vid.toUpperCase()=="YES"){
     var talent =prompt("Do you need to provide the actors for the video?(Enter Y or N)");}
     else {talent="N";}
     if (((pics.toUpperCase()=="Y"||pics.toUpperCase()=="YES")&&(vid.toUpperCase()=="Y"||vid.toUpperCase()=="YES"))&&(talent.toUpperCase()=="Y"||talent.toUpperCase()=="YES")){
-    console.log("For this job, you will need the following people. "+perTwo+", "+perSix+", "+perFour+".");
+    console.log("For this job, you will need the following people. "+perTwo+", "+perSix+", "+perFour+", "+perThree+".");
     }else if (((pics.toUpperCase()=="N"||pics.toUpperCase()=="NO")&&(vid.toUpperCase()=="N"||vid.toUpperCase()=="NO"))&&(talent.toUpperCase()=="N"||talent.toUpperCase()=="NO")){
         console.log("For this job you will only need a "+perTwo+".");
     }else if (((pics.toUpperCase()=="N"||pics.toUpperCase()=="NO")&&(vid.toUpperCase()=="Y"||vid.toUpperCase()=="YES"))&&(talent.toUpperCase()=="N"||talent.toUpperCase()=="NO")){
-        console.log("For this job you will need a "+perTwo+" and a "+perFour+".");}
-    else if (((pics.toUpperCase()=="Y"||pics.toUpperCase()=="YES")&&(vid.toUpperCase()=="N"||vid.toUpperCase()=="NO"))&&(talent.toUpperCase()=="N"||talent.toUpperCase()=="NO")){
-        console.log("For this job you will need a"+perTwo+" and a "+perFour+".");}
+        console.log("For this job you will need a "+perTwo+" and a "+perFour+".");
+    }else if (((pics.toUpperCase()=="Y"||pics.toUpperCase()=="YES")&&(vid.toUpperCase()=="N"||vid.toUpperCase()=="NO"))&&(talent.toUpperCase()=="N"||talent.toUpperCase()=="NO")){
+        console.log("For this job you will need a "+perTwo+" and a "+perThree+".");
+    }else if (((pics.toUpperCase()=="Y"||pics.toUpperCase()=="YES")&&(vid.toUpperCase()=="Y"||vid.toUpperCase()=="YES"))&&(talent.toUpperCase()=="N"||talent.toUpperCase()=="NO")){
+    console.log("For this job you will need a "+perTwo+" and a "+perFour+", "+perThree+".");}
 
-else {talent="N"}
 } else if (parseInt(jobType)==2){
     var pics =prompt("Does the client need you to produce the pictures of his products? (Enter Y or N)");
     var vid =prompt("Does the client need you to shoot the informational video?");
     if (vid.toUpperCase()=="Y"||vid.toUpperCase()=="YES"){
-        var talent =prompt("Do you need to provide the actors for the video?(Enter Y or N)");
-    } else {talent="N"}
-    var logo =prompt("Does the client need original graphics made for the site?");
+        var talent =prompt("Do you need to provide the actors for the video?(Enter Y or N)");}
+    else {talent="N";}
+    if (((pics.toUpperCase()=="Y"||pics.toUpperCase()=="YES")&&(vid.toUpperCase()=="Y"||vid.toUpperCase()=="YES"))&&(talent.toUpperCase()=="Y"||talent.toUpperCase()=="YES")){
+        console.log("For this job, you will need the following people. "+perTwo+", "+perSix+", "+perFour+", "+perThree+", "+perFive+".");
+    }else if (((pics.toUpperCase()=="N"||pics.toUpperCase()=="NO")&&(vid.toUpperCase()=="N"||vid.toUpperCase()=="NO"))&&(talent.toUpperCase()=="N"||talent.toUpperCase()=="NO")){
+        console.log("For this job you will only need a "+perTwo+" and a "+perFive+".");
+    }else if (((pics.toUpperCase()=="N"||pics.toUpperCase()=="NO")&&(vid.toUpperCase()=="Y"||vid.toUpperCase()=="YES"))&&(talent.toUpperCase()=="N"||talent.toUpperCase()=="NO")){
+        console.log("For this job you will need a "+perTwo+" and a "+perFour+", "+perFive+".");
+    }else if (((pics.toUpperCase()=="Y"||pics.toUpperCase()=="YES")&&(vid.toUpperCase()=="N"||vid.toUpperCase()=="NO"))&&(talent.toUpperCase()=="N"||talent.toUpperCase()=="NO")){
+        console.log("For this job you will need a "+perTwo+" and a "+perThree+", "+perFive+".");
+    }else if (((pics.toUpperCase()=="Y"||pics.toUpperCase()=="YES")&&(vid.toUpperCase()=="Y"||vid.toUpperCase()=="YES"))&&(talent.toUpperCase()=="N"||talent.toUpperCase()=="NO")){
+        console.log("For this job you will need a "+perTwo+" and a "+perFour+", "+perThree+", "+perFive+".");}
+    var logo =prompt("Does the client need original graphics made for the site?(Enter Y or N");
+    if (logo.toUpperCase()=="Y"||logo.toUpperCase()=="YES"){
+        console.log("You will also need a "+perOne+".");
+    }
     }
   else if (parseInt(jobType)==3){
-    var pics =prompt("Does the client need you to produce the pictures of his products? (Enter Y or N)");
-
+    var logo =prompt("Does the client need original graphics made for the site?(Enter Y or N");
+    if (logo.toUpperCase()=="Y"||logo.toUpperCase()=="YES"){
+        console.log("You will need a "+perOne+" and a "+perTwo+".");
+    }else console.log("You will only need a "+perTwo+" for this job.")
 }
 
 
