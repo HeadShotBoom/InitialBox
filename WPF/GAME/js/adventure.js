38ded7f3e324a29b576a736e0777a535149e3e20
HeadShotBoom <DCarroll@Fullsail.edu>
2013-07-29 16:32:16 -0400

    Class Progress

--- WPF/GAME/js/adventure.js
+++ WPF/GAME/js/adventure.js
@@ -3,7 +3,7 @@
 //Volcano = 50%
 //Acid = 10%
 
-var world=[];
+var world=[[],[]];
 function landType(){
     var counter = Math.floor(100*Math.random()+1);
     if (counter<=20){
@@ -12,12 +12,10 @@ function landType(){
     var theLand="Caves";
     }else if (counter<=90 && counter>40){
         var theLand="Volcano";
-    }else if (counter<=99 && counter>90){
+    }else if (counter<=100 && counter>90){
         var theLand="Acid";
-    }else if (counter=100){
-        var theLand="FINISH"
+    }
 
-}
 return (theLand);
 
 }
@@ -27,22 +25,38 @@ return (theLand);
 function makeGrid(){
     var counter = Math.floor(100* Math.random());
     var dirtType ='';
-    for(i=0;i<3;i++){
-        world[i]=[];
-        for(j=0;j<3;j++){
-        world[i][j]=landType();
-        }console.log(world[i])}
+    for(h=0;h<1;h++){
+
+        for(i=0;i<10;i++){
+            world[h][i]=[];
+        }
+    }
+
+    for(i=0;i<10;i++){
+        for(j=0;j<10;j++){
+            world[0][i][j]=landType();
+
+        }
+    }
+
+
+    world[0][0][0]='START';
+    world[0][9][9]='FINISH';
+    for(i=0;i<10;i++){
+    console.log(world[0][i]);
+}
 
 }
 makeGrid();
 
+/*
 var Y = 0;
 var X = 0;
-var positionChange=''
-var playGame = "ON"
+var positionChange='';
+var playGame = "ON";
 while (playGame=='ON'){
     var positionChange=prompt("Which direction would you like to move? Use standard WASD movement keys... Type QUIT to end the game.");
-if (positionChange.toUpperCase()=='W'&& Y>0){
+if (positionChange.toUpperCase()=='W'){
     Y--;
 }else if (positionChange.toUpperCase()=='S'){
     Y++;
@@ -56,7 +70,8 @@ if (positionChange.toUpperCase()=='W'&& Y>0){
     if (Y<0){Y=2;}
     if (X==3){X=0;}
     if (X<0){X=2;}
-var positionNow = world[Y][X];
+var positionNow = world[0][Y][X];
     console.log(positionNow);
 }
 alert("You have ended the game.");
+ */
--- WPF/GAME/js/creatures.js
+++ WPF/GAME/js/creatures.js
@@ -1,6 +1,39 @@
-// This will generate creatures to populate our world.
+
+var creatureBaseStatProto = [];
+//player=0, SSS=1, Sexy Toad=2, Death Leach=3, Horse=4,
+creatureBaseStatProto[0]=[0,20,1,.5,.01,0,'Player',[1,1]];
+creatureBaseStatProto[1]=[0,5,2,.2,.2,1,'Slimy Slippery Snake',[1,3]];
+creatureBaseStatProto[2]=[0,2,2,.2,.05,1,'Sexy Toad',[1,5]];
+creatureBaseStatProto[3]=[0,5,.1,.9,.991,1, 'Death Leach',[1,1]];
+creatureBaseStatProto[4]=[50,1000,10,.5,0,1,'Dragon',[1,10]];
+var player =0;
+var sss =1;
+var sexyToad=2;
+var deathLeach=3;
+var dragon=4;
+var armor=0;
+var health=1;
+var damage=2;
+var startHit=3;
+var startDodge=4;
+var startXp=5;
+var title=6;
+var horde=7;
+var hordeMin=0;
+var hordeMax=1;
+
+
+
+function makeHorde (character){
+    var hordeSize=Math.random()*(creatureBaseStatProto[character][horde][hordeMax]-creatureBaseStatProto[character][horde][hordeMin])+creatureBaseStatProto[character][horde][hordeMin];
+    for(i=hordeSize;i>0;i--);{
+    return (hordeSize);
+} }
 
 //This function allows me to adjust the starting value of any stat dynamically
+
+
+
 function varyValue(N,M){
     var rangeOfvariance = (Math.floor(Math.random()*100)+1)/100;
     var posOrNeg = Math.floor((Math.random()*10)+1);
@@ -14,11 +47,10 @@ var arrayWithStatsPass=[];
 arrayWithStatsPass[0]='Monster';
 arrayWithStatsPass[1]='Plains';
 //**************************Remove this when testing is complete
+
+
 function makeCreature()
 {
-
-
-
 var playerOrMonster=arrayWithStatsPass[0];
 if (playerOrMonster=='Player'){
     var stats=[];
@@ -29,7 +61,9 @@ if (playerOrMonster=='Player'){
     stats[4]=0.01;//starting %dodge = 1%
     stats[5]=1;////starting experience =0
     stats[6]='PlayerCharacterName'; // character name
-
+    stats[7]="";//Armor
+    stats[8]="";//Weapon
+    stats[9]="";//Treasure
 }
 if (playerOrMonster=='Monster'){
     var stats=[];
@@ -45,6 +79,10 @@ if (playerOrMonster=='Monster'){
             stats[4]=.2;//starting %dodge
             stats[5]=1;//starting experience
             stats[6]='Slimy sneaky slippery snake';// character name
+            stats[7]="";//Armor
+            stats[8]="";//Weapon
+            stats[9]="";//Treasure
+            stats[10]=3;//horde Size
             break;
             }
             else if (animalTypeSwamp>=.1 && animalTypeSwamp<=.9){
@@ -56,6 +94,10 @@ if (playerOrMonster=='Monster'){
             stats[4]=.05;//starting %dodge
             stats[5]=1;//starting experience
             stats[6]='Sexy Toad';// character name
+            stats[7]="";//Armor
+            stats[8]="";//Weapon
+            stats[9]="";//Treasure
+            stats[10]=10;//horde Size
             break;
             }
             else {
@@ -67,20 +109,18 @@ if (playerOrMonster=='Monster'){
             stats[4]=.991;//starting %dodge
             stats[5]=1;//starting experience
             stats[6]='Death Leach'; // character name
+            stats[7]="";//Armor
+            stats[8]="";//Weapon
+            stats[9]="";//Treasure
+            stats[10]=2;//horde Size
             break;
             }
 
         case "Plains":
             var dragonOrHorse =Math.random();
-            if (dragonOrHorse<.01){
+            if (dragonOrHorse<.999){
             //This case will be dragon
-                stats[0]=50;//starting armor value =0
-                stats[1]=1000;//starting health
-                stats[2]=10; //starting damage
-                stats[3]=.5; //starting %hit
-                stats[4]=0;//starting %dodge
-                stats[5]=1;//starting experience
-                stats[6]='Fat Sad Dragon';// character name
+                var monsterNow = makeHorde(dragon);
                 break;
             }
             else {
@@ -92,21 +132,19 @@ if (playerOrMonster=='Monster'){
                 stats[4]=.1;//starting %dodge
                 stats[5]=1; //starting experience
                 stats[6]='A left wing horse of a different color';// character name
+                stats[7]="";//Armor
+                stats[8]="";//Weapon
+                stats[9]="";//Treasure
+                stats[10]=4;//horde Size
                 break;
             }
     }
 
-}return stats;}
+}return monsterNow;}
 
 
-var playerCharacter = makeCreature(arrayWithStatsPass);
-console.log(playerCharacter[6]);
-console.log(playerCharacter[0]);
-console.log(playerCharacter[1]);
-console.log(playerCharacter[2]);
-console.log(playerCharacter[3]);
-console.log(playerCharacter[4]);
-console.log(playerCharacter[5]);
+var makeIt = makeCreature(arrayWithStatsPass);
+console.log(makeIt);
 
 
 
