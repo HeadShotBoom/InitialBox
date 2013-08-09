9340b14851ad53da56eb3464f64adb747ea02a1c
HeadShotBoom <DCarroll@Fullsail.edu>
2013-08-08 23:35:27 -0400

    Lots of work

--- PWA1/Wk1/goal2_assign_jspractice/js/main.js
+++ PWA1/Wk1/goal2_assign_jspractice/js/main.js
@@ -1,5 +1,5 @@
 // self-executing function
-(function(){
+
 
     /*******************************************
 
@@ -45,48 +45,110 @@
     console.log('------ Goal2: Assignment: JavaScript Practice ----------');
 
     console.log("1. avg of an array of numbers");
-    var avgNumbers = function(arr){
+    var nums = [1,2,3,4,5,6,7,8,9,10];
+    function avgNumbers(arr){
+        var total = 0;
+        for(i=0;i<arr.length;i++){
+            total=total+arr[i];
+        }
+       var mean = total/(arr.length);
+       console.log("Mean number is "+mean);
+    }
+avgNumbers(nums);
 
-        //CODE GOES HERE
-    };
 
-    console.log('avg number = ', avgNumbers([1,2,3,4,5]));
 
     //--------------------------------------------------------
     console.log("2. concat first and last name");
-
+    function fullName(X,Y){
+        console.log(X, Y);
+    }
         //PUT FUNCTION HERE
 
-    console.log(fullName('James', 'Bond'));
+fullName('James', 'Bond');
 
     //--------------------------------------------------------
     console.log("3. word count");
-    var ipsum = "this is test text that is being used as input to a function"
+    var ipsum = "this is test text that is being used as input to a function";
 
-        //PUT FUNCTION HERE
+function wordCount(passThrough){
+        var splitIt = passThrough.split(" ");
+        var numOfWords = splitIt.length;
+    console.log(numOfWords);
 
-    console.log(wordCount(ipsum));
-
+    }
+    wordCount(ipsum);
     //--------------------------------------------------------
     console.log("4. sentence char count");
+function charCount(passThrough){
+    var newWords = passThrough.trim();
+    var length = newWords.length;
+    console.log(length);
+}
 
         //PUT FUNCTION HERE
 
-    console.log(charCount(ipsum));
+    charCount(ipsum);
 
     //--------------------------------------------------------
     console.log("5. how many vowels in a word");
 
-        //PUT FUNCTION HERE
+function vowelsInWord(X){
+    count = 0;
+    var capitalLetters = X.toUpperCase();
+    posa = capitalLetters.indexOf("A");
+    while ( posa != -1 ) {
+    count++;
+    posa =capitalLetters.indexOf("A",posa+1);
 
-    console.log(vowelsInWord('JavaScript'));
+}
+    pose = capitalLetters.indexOf("E");
+    while ( pose != -1 ) {
+        count++;
+        pose = capitalLetters.indexOf("E",pose+1);
+}
+    posi = capitalLetters.indexOf("I");
+    while ( posi != -1 ) {
+        count++;
+        posi = capitalLetters.indexOf("I",posi+1);
+    }
+    poso = capitalLetters.indexOf("O");
+    while ( poso != -1 ) {
+        count++;
+        poso = capitalLetters.indexOf("O",poso+1);
+    }
+    posu = capitalLetters.indexOf("U");
+    while ( posu != -1 ) {
+        count++;
+        posu = capitalLetters.indexOf("U",posu+1);
+    }console.log(count)}
+
+vowelsInWord('JavaScript');
 
     //--------------------------------------------------------
     console.log("6. find number and create an array of even or odd numbers");
+    var findNum = function(array,X){
+        var evenNumbers=[];
+        var oddNumbers=[];
+        if (X==false){
+            for(i=0;i<array.length;i++){
+                if (array[i]%2!=0){
+                    oddNumbers.push(array[i]);
+                }
+
+            }return oddNumbers;
+
+        }else{
+        for(i=0;i<array.length;i++){
+            if (array[i]%2==0){
+                evenNumbers.push(array[i]);
+            }
+
+        }return evenNumbers;
+    }}
 
         //PUT FUNCTION HERE
 
     console.log(findNum([31,22,4,67,83,6,5,4]));
     console.log(findNum([31,22,4,67,83,6,5,4], false));
 
-})();
