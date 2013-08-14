b5884c2eee050d2a77a684e92160292652772fce
HeadShotBoom <DCarroll@Fullsail.edu>
2013-08-13 22:23:33 -0400

    Progress

---
+++ PWA1/Wk1/goal4_assign_duel3/js/main.js
@@ -0,0 +1,49 @@
+// Script
+
+//Define player variables IAW FSO directions.
+
+fighters = [playerOne = {name:"Scott",health:100,damage:50},playerTwo = {name:"Vid",health:100,damage:50}];
+
+//store round number in a variable
+var round = 0;
+
+
+//Create function that interacts with player health, simulating combat.
+function fight(){
+    console.log("FIGHT!");
+
+
+    //calculate amount of damage each player will inflict.
+    var minimumDamageP1 = fighters[0].damage * .5;
+    var minimumDamageP2 = fighters[1].damage * .5;
+    var player1ActualDamage = Math.floor(Math.random()*(fighters[0].damage-minimumDamageP1)+minimumDamageP1);
+    var player2ActualDamage = Math.floor(Math.random()*(fighters[1].damage-minimumDamageP2)+minimumDamageP2);
+
+    //inflict damage
+    fighters[0].health=fighters[0].health-player1ActualDamage;
+    fighters[1].health=fighters[1].health-player2ActualDamage;
+
+
+    console.log(fighters[0].name+": "+fighters[0].health + " " + fighters[1].name+":"+fighters[1].health);
+
+    }
+var navLinks = document.querySelector('#fight_btn a');
+navLinks.onclick = function(e){
+    fight();
+    winnerCheck();
+};
+
+function winnerCheck(){
+    var result="no winner";
+    if (fighters[0].health<1 && fighters[1].health<1)
+    {
+        result = "You Both Die";
+    } else if(fighters[0].health[1]<1){
+        result =fighters[1].name+" WINS!!!"
+    } else if (fighters[1].health<1)
+    {
+        result = fighters[0].name+" WINS!!!"
+    }
+    console.log(result);
+}
+
