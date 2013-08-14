e95669d1f2280e2343d857246622ec8522006be0
HeadShotBoom <DCarroll@Fullsail.edu>
2013-08-14 02:00:07 -0400

    Finished Game

--- PWA1/Wk1/goal4_assign_guessing_game/js/main.js
+++ PWA1/Wk1/goal4_assign_guessing_game/js/main.js
@@ -10,18 +10,48 @@
 //Game variables
 
 (function (){
+    var round = 0;
+    var roundLeft ='';
 
     function correct(){
+        round++;
+        roundLeft = 3-round;
         var randomNumber = Math.floor(Math.random()*10+1);
-        console.log(randomNumber);
         var playerEntry = document.querySelector('#input').value;
-        console.log(playerEntry);
         var playerEntryToInterger = parseInt(playerEntry);
-        console.log(playerEntryToInterger);
 
+        var title = document.querySelector('#output');
+        if (playerEntryToInterger>randomNumber){
+            title.innerHTML = 'Your guess is higher than my number. You have '+roundLeft+' guesses left';
+        }
+        if (playerEntryToInterger<randomNumber){
+            title.innerHTML = 'Your guess is lower than my number. You have '+roundLeft+' guesses left';
+        }
+        if (playerEntryToInterger==randomNumber){
+            title.innerHTML = 'Your guess is correct, You win!';
+        }
+        if (roundLeft==0){
+            title.innerHTML = 'You have run out of guesses. GAME OVER!';
+        }
+        if(roundLeft==0){
+            playerEntryAccept[0].removeEventListener('click', correct, false);
+
+        }
     }
 
- var playerEntryAccept = document.getElementsByTagName('button');
+
+
+    var playerEntryAccept = document.getElementsByTagName('button');
     playerEntryAccept[0].addEventListener('click', correct, false);
+    var pressEnter = document.querySelector('#input');
+        pressEnter.addEventListener('keypress',function(e){
+            if (13 == e.keyCode && roundLeft!==0){
+                correct();
+            }
+        })
+
+
+
+
 
 })();
