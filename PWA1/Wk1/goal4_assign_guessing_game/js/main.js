b32b663f92c5827576ec8a5769664c03b15a7bd5
HeadShotBoom <DCarroll@Fullsail.edu>
2013-08-14 00:19:02 -0400

    Progress

--- PWA1/Wk1/goal4_assign_guessing_game/js/main.js
+++ PWA1/Wk1/goal4_assign_guessing_game/js/main.js
@@ -1,14 +1,27 @@
 /**
  * GUESSING GAME:
  *
- * Created By: 
- * Date: 
+ * Created By:Daniel Carroll
+ * Date:13 Aug 2013
  * 
  * GUESSING GAME
  */
 
 //Game variables
-(function ()
 
+(function (){
+
+    function correct(){
+        var randomNumber = Math.floor(Math.random()*10+1);
+        console.log(randomNumber);
+        var playerEntry = document.querySelector('#input').value;
+        console.log(playerEntry);
+        var playerEntryToInterger = parseInt(playerEntry);
+        console.log(playerEntryToInterger);
+
+    }
+
+ var playerEntryAccept = document.getElementsByTagName('button');
+    playerEntryAccept[0].addEventListener('click', correct, false);
 
 })();
