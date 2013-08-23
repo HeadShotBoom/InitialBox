9e2f78aea07fa6080a528d43543a31f860639653
HeadShotBoom <DCarroll@Fullsail.edu>
2013-08-22 22:34:15 -0400

    Done!

--- PWA1/Wk1/goal7_assign_obj_to_prototypes/js/main.js
+++ PWA1/Wk1/goal7_assign_obj_to_prototypes/js/main.js
@@ -5,6 +5,47 @@
  */
 // Daniel CarrollAug 22, 2013
 
-name = ['Dan Carroll','Lady Gaga','Heraldo Rivera','Vladimere Putin','The All Mighty'];
 
-var person = [];
+(function(){
+    var numOfPeople=3;
+    var people=[];
+    var names = ["Scott","Lyndon","Orcun","Eric","Rebecca"];
+    var interval;
+
+    //Loop that instantiates 3 Person objects
+    for (var i=0; i<numOfPeople;i++){
+        //will generate a number up to the names.length which is 5
+        var personIndex = Math.floor(Math.random()*names.length);
+        var person = new Person(names[personIndex],i+1);
+
+        populateHTML(person.name,"r"+(i+1)+"c1");
+        populateHTML(person.job,"r"+(i+1)+"c2");
+
+        people.push(person);
+        names.splice(personIndex,1);
+
+    }
+
+    //ensure only one interval is running - clear out the old before running the new
+    clearInterval(interval);
+
+    //sets interval to 30FPS
+    //The interval() method will continue calling the function until the clearInterval() is called,
+    //or the window is closed.
+    //the ID value returned by setInterval() is used as the parameter for the clearInterval() method.
+
+    interval=setInterval(runUpdate, 1000/30);
+
+    function populateHTML(data,field){
+        var id=document.getElementById(field);
+        id.innerHTML = data;
+    }
+    function runUpdate(){
+        people.forEach(function(element){
+            //console.log(element);
+            //element = the items in the people array
+            //.update = the prototype method in the person.js file
+            element.update();
+        });
+    }
+})();
--- PWA1/Wk1/goal7_assign_obj_to_prototypes/js/person.js
+++ PWA1/Wk1/goal7_assign_obj_to_prototypes/js/person.js
@@ -6,31 +6,45 @@
 
 
 (function(){
+    //add the person object to the global window object - because main.js needs it
+    window.Person=Person;
 
-    window.person=Person;
+    //defines all possible job choices and actions for all the Person instances
 
     Person.jobs = ['Teacher','Farmer','Student','Pilot'];
     Person.actions = ['Sleeping','Eating','Working'];
 
     function Person(name,row){
-        this.name = name;
-        //action and job are selected randomly from the person.jobs array and the person actions array
+        console.log("person created: ", name);
+        this.name=name;
 
-        this.job=Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
-        this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
+        //sets initial action for each person
+        this.action=Person.actions[Math.floor(Math.random()*Person.actions.length)];
+
+        //sets job
+        this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
+
+        //defines row so JS knows what row in the HTML to update the text in
         this.row=row;
+
+        //displays initial action
+        var id = document.getElementById("r"+this.row+"c3");
+        id.innerHTML=this.action;
     }
 
+    //Persons update method. This function occurs 30 times a second
+    //and determines if the action changers for each person
+
     Person.prototype.update=function(){
-        this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
-    };
+        if(Math.floor(Math.random() <.01)){
+            var i = Math.floor(Math.random()*Person.actions.length);
+            this.action=Person.actions[i];
+            var id = document.getElementById("r"+this.row+"c3");
+            id.innerHTML=this.action;
+
+        }
 
 
 
-
-
-
-
-
-
+    }
 })();
