a20d8d5fde96ecbcaa3b66d033b6fae95b5349a1
HeadShotBoom <DCarroll@Fullsail.edu>
2013-08-25 20:45:46 -0400

    Replace File

--- PWA1/Wk1/goal8_assign_easyLibrary/js/ryu.js
+++ PWA1/Wk1/goal8_assign_easyLibrary/js/ryu.js
@@ -1,19 +1,53 @@
 /*
 	PWA1: Goal7:  Simple Library
+// the code below is how you would call the library function from the main javascript file
+
+	var test = document.querySelectorAll("#test a");  //stores the anchor tags to the variable 'test'
+	
+	// call the library, passing the variable 'test' into it
+    NAMEofLIBRARY(test).each(function(){
+
+    // any additional code to run would go here
+
+    });
+   
+    
+// But, if you break it up so that it is stored to a variable,
+// it would look something like this..
+	
+	var someVariable = NAMEofLIBRARY(test);
+
+// Then you can call on the functions of the library like so,
+
+	someVariable.each(function(){
+		//any additional code would go in here
+	});
+
 */
 
-var YOURLIBNAME = function(){
+// NAMEofLIBRARY is arbitrary, you can name it whatever you want
 
-    return new YOURLIBNAME.prototype.init();
+var NAMEofLIBRARY = function(elems){
+
+    return new NAMEofLIBRARY.prototype.init(elems);
 
 };
 
-YOURLIBNAME.prototype = {
 
-    init: function(){},
+NAMEofLIBRARY.prototype = {
 
-    elements: []
+    init: function(elems){
+        this.elements = elems; // pass the existing array
+    },
+    elements: [],
+    each: function(callback){
+        for(i=0, max = this.elements.length; i< max; i++){
+            callback.call(this.elements[i]);
+            console.log(this.elements[i]);
+
+        }
+    }
 
 }; // end prototype
 
-YOURLIBNAME.prototype.init.prototype = YOURLIBNAME.prototype;
+NAMEofLIBRARY.prototype.init.prototype = NAMEofLIBRARY.prototype;
