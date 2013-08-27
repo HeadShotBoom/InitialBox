0cbe34b63f527bd0b5826991ef75eac61147a88c
HeadShotBoom <DCarroll@Fullsail.edu>
2013-08-27 17:32:08 -0400

    DAY NINE!!!

---
+++ PWA1/Work From Instructor/day 9/canvas/js/main.js
@@ -0,0 +1,12 @@
+console.log("start canvas");
+
+(function(){
+    
+    var canvas = document.createElement("canvas");//Creates a canvas element programmatically
+    document.body(appendChild(canvas));//adds the canvas to the DOM
+    var ctx = canvas.getContext("2d");//gain access to the canvas API
+    canvas.width = 400;
+    canvas.height = 400;
+
+    
+})();
