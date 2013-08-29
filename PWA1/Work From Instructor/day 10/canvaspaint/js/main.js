9505fad25a698a3d7dd12b15019f21f6323d7e42
HeadShotBoom <DCarroll@Fullsail.edu>
2013-08-29 18:37:49 -0400

    Done with canvas draw

--- PWA1/Work From Instructor/day 10/canvaspaint/js/main.js
+++ PWA1/Work From Instructor/day 10/canvaspaint/js/main.js
@@ -2,7 +2,39 @@
 
 (function() {
     var canvas = document.createElement("canvas");
-    document.
+    document.body.appendChild(canvas);
+    canvas.width = 640;
+    canvas.height = 480;
+    var ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = '#cccccc';//set background color
+    ctx.fillRect(0,0,640,480);
+
+    var mouse = utils.getMouse(canvas);//the getmouse function which returns an object literal that has X and Y of the Mouse
+    var draw = false;//boolean that holds the status of if the mouse is being clicked.
+
+    var color = "rgb(255,0,0)";
+    ctx.strokeStyle = color;
+
+    canvas.addEventListener("mousedown", onDraw);
+    canvas.addEventListener("mouseup", onStopDraw);
+    canvas.addEventListener("mouseout", onStopDraw);
+
+    function onDraw(e){
+        draw=true;
+        ctx.beginPath();
+        ctx.moveTo(mouse.x,mouse.y);
+    }
+    function onStopDraw(e){
+        draw=false;
+    }
+    (function update(){
+        if(draw){
+            ctx.lineTo(mouse.x,mouse.y);
+            ctx.stroke();
+        }
+        requestAnimationFrame(update);
+    })();
    
 
 })();
