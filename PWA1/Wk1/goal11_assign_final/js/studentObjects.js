9429f403dce8047f5224390bfd5c309c135c95e3
HeadShotBoom <DCarroll@Fullsail.edu>
2013-08-31 17:56:01 -0400

    Final, some progress

---
+++ PWA1/Wk1/goal11_assign_final/js/studentObjects.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: DCarroll
+ * Date: 8/31/13
+ * Time: 5:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var Student = function(options){
+    this.name = options.name;
+    this.address = options.address;
+    this.gpa = options.gpa;
+    this.phone = options.phone;
+    this.date = options.date;
+};
